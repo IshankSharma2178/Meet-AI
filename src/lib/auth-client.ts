b2ca73450535b9543c26aa5a6777748a0ae95a3e
IshankSharma2178 <ishank.sharma.9891@gmail.com>
2026-04-17 10:59:06 +0530

    27: fixing auth url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,6 +2,6 @@ import { createAuthClient } from "better-auth/react";
 import { polarClient } from "@polar-sh/better-auth";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_AUTH_URL,
   plugins: [polarClient()],
 });
